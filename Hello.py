22631d85ddd3553c162a0f1fe6e8907339914a02
Tomer Hambra <73949014+TomerHambra@users.noreply.github.com>
2023-09-28 07:10:06 +0300

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -4,10 +4,6 @@ from selenium import webdriver
 from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
-import streamlit as st
-from streamlit.logger import get_logger
-
-LOGGER = get_logger(__name__)
 
 def run():
     
