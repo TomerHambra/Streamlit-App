0942ecc952094df3bf99fd8083f314fefaf59588
Tomer Hambra <73949014+TomerHambra@users.noreply.github.com>
2023-09-28 08:09:20 +0300

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -27,8 +27,16 @@ def run():
     if day and hour:
         if hour == 15:
             hour = 0
+
+        
         url = 'https://beitbiram.iscool.co.il/default.aspx'
-        browser = webdriver.Chrome()
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--no-sandbox")
+        browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+        
         browser.get(url)
         grade = '//*[@id="dnn_ctr7126_TimeTableView_ClassesList"]/option['
     
