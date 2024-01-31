7a2f2f4ee36aba7c7a92ac028c88e713c2214158
Tomer Hambra <73949014+TomerHambra@users.noreply.github.com>
2024-01-31 10:36:04 +0200

    Update RoomFinder.py

--- RoomFinder.py
+++ RoomFinder.py
@@ -155,7 +155,7 @@ def get_available_classes_on_date(htmls: list[str], day: int, hour: int, bar, ro
     for html in htmls:
         soup = BeautifulSoup(html, "lxml")
         table = soup.find("table", {"class": "TTTable"})
-        if table == None: print(html)
+        print(html)
         row = table.find_all("tr", {"valign": "top"})[hour]
         cell = row.find_all("td", {"class": "TTCell"})[day]
         available_classes -= get_taken_classes_on_date(cell)
